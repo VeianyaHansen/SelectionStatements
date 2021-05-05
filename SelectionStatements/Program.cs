ee181809a5af071cc4984e773124aa0e2742350f
Veianya Hansen <melaya0105@gmail.com>
2021-05-05 08:20:36 -0700

    Add project files.

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int favoriteNumber = 5;
+            //Console.WriteLine("Guess my favorite number (between 1-10):");
+            //int userGuess = int.Parse(Console.ReadLine());
+
+            //var r = new Random();
+            //var favoriteNumber = r.Next(1, 10);
+
+            //var userGuess = int.Parse(Console.ReadLine());
+
+            //if (userGuess < favoriteNumber)
+            //{
+            //    Console.WriteLine("Your guess was too low");
+            //}
+            //else if (userGuess > favoriteNumber)
+            //{
+            //    Console.WriteLine("Your guess was too high");
+            //}
+            //else if (userGuess == favoriteNumber)
+            //{
+            //    Console.WriteLine("You guessed it!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nevermind");
+            //}
+
+            /***********************************************************/
+
+            //Console.WriteLine("Choose a subject");
+            //string userSubject = Console.ReadLine().ToLower();
+
+            //switch (userSubject)
+            //{
+            //    case "english":
+            //        Console.WriteLine("Your favorite subject is English.");
+            //        break;
+            //    case "math":
+            //        Console.WriteLine("Your favorite subject is Math.");
+            //        break;
+            //    case "science":
+            //        Console.WriteLine("Your favorite subject is Science.");
+            //    break;
+            //case "history":
+            //    Console.WriteLine("Your favorite subject is History.");
+            //    break;
+            //case "pe":
+            //    Console.WriteLine("Your favorite subject is PE.");
+            //    break;
+            //default:
+            //    Console.WriteLine("Something went wrong.");
+            //    break;
+            //}
+
+           
+
+        }
+    }
+}
